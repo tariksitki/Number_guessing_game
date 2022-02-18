72aebff83b2a7f771d7e6bbfea4cc445a581ad42
tarik <63872091+tariksitki@users.noreply.github.com>
2022-02-18 16:46:26 +0100

    completed

--- app.js
+++ app.js
@@ -1,52 +1,159 @@
 
+function myRandom() {
+    randomNumber = (Math.round(Math.random() * 99) + 1);
+    return randomNumber;
+}
 
-let number = 32;
+let number;
+
+window.addEventListener("load", () => {
+    number = myRandom();
+    console.log(number);
+})
 
 let user_number = document.getElementById("user_number");
-
 let submit = document.getElementById("submit");
-
 let output_span = document.getElementById("output_span");
-
 let attempts = document.getElementById("attempts_span");
-
-let counter = 0;
-
 let reset = document.getElementById("reset");
+let info = document.getElementById("info")
+
+// let counter = 0;
+
+// submit.addEventListener("click", () => {
+
+//     if (user_number.value == number) {
+//         counter += 1;
+//         output_span.style.display = "block";
+//         output_span.innerText = "Congrats!!! You found the number!";
+//         attempts.innerHTML = "Number of Attempts: " + counter;
+
+//     } else if (user_number.value > number) {
+//         output_span.style.display = "block";
+//         output_span.innerHTML = "Enter a smaller number!";
+//         counter += 1;
+//         attempts.innerHTML = "Number of Attempts: " + counter;
+
+//     } else if (user_number.value < number) {
+//         output_span.style.display = "block";
+//         output_span.innerText = "Enter a bigger number!";
+//         counter += 1;
+//         attempts.innerHTML = "Number of Attempts: " + counter;
+//     }  
+
+// });
+
+
+// reset.addEventListener("click", () => {
+//     counter = 0;
+//     attempts.innerHTML = "Number of Attempts: " + counter;
+//     output_span.style.display = "none";
+//     user_number.value = 0;
+// })
+
+
+//     //  web page load, auto focus
+
+// user_number.focus();  
+
+let counter = 7;
+let big_number = 100;
+let little_number = 0;
+let attempted_array = [];
+
 
 submit.addEventListener("click", () => {
 
-    
+    if (user_number.value > 100 || user_number.value < 0) {
+        alert(`Please enter a number between ${little_number} and ${big_number}`);
 
-    if (user_number.value == number) {
-        counter += 1;
-        output_span.style.display = "block";
-        output_span.innerText = "Congrats!!! You found the number!";
-        attempts.innerHTML = "Number of Attempts: " + counter;
+    } else if (user_number.value == "") {
+        alert(`Please enter a number between ${little_number} and ${big_number}`);
+
+    } else if (user_number.value == number) {
+        counter -= 1;
+        // output_span.style.display = "block";
+        info.innerText = "Congrationals! You found the number!";
+        attempts.innerHTML = `You found the Number in ${7-counter} Tries`;
 
     } else if (user_number.value > number) {
-        output_span.style.display = "block";
-        output_span.innerHTML = "Enter a smaller number!";
-        counter += 1;
-        attempts.innerHTML = "Number of Attempts: " + counter;
+        counter -= 1;
+
+        if (user_number.value <= big_number) {
+            big_number = user_number.value;
+            info.innerHTML = `Enter a number between ${little_number} and ${big_number}`;
+            attempts.innerHTML = `You have the right to try ${counter} Times`;
+            attempted_array.push(user_number.value);
+
+        } else if (user_number.value > big_number) {
+            big_number = big_number;
+            info.innerHTML = `Enter a number between ${little_number} and ${big_number}`;
+            attempts.innerHTML = `You have the right to try ${counter} Times`;
+            attempted_array.push(user_number.value);
+        }   
+        
 
     } else if (user_number.value < number) {
-        output_span.style.display = "block";
-        output_span.innerText = "Enter a bigger number!";
-        counter += 1;
-        attempts.innerHTML = "Number of Attempts: " + counter;
+        counter -= 1;
+        // output_span.style.display = "block";
+
+        if (user_number.value <= little_number) {
+            little_number = little_number;
+            info.innerHTML = `Enter a number between ${little_number} and ${big_number}`;
+            attempts.innerHTML = `You have the right to try ${counter} Times`;
+            attempted_array.push(user_number.value);
+        }   else {
+            little_number = user_number.value;
+            info.innerHTML = `Enter a number between ${little_number} and ${big_number}`;
+            attempts.innerHTML = `You have the right to try ${counter} Times`;
+            attempted_array.push(user_number.value);
+        }
     }  
+    
+    
+    
+    
+    if (counter == 0 && user_number.value != number) {
+                reset = document.getElementById("reset");
+                info.innerHTML = "Game Over!";
+                submit.disabled = true;
+                attempts.innerHTML = "";
+                reset.value = "Play Again";
+                // output_span.innerHTML = attempted_array.value;
+
+    } else if (counter == 0 && user_number.value == number) {
+                info.innerText = "Congrationals! You found the number!";
+                attempts.innerHTML = `You found the Number in ${7-counter} Tries`;
+                // output_span.innerHTML = attempted_array.value;
+            }
+        output_span.style.display = "block";
+        output_span.innerHTML = `( You have tried These Numbers:  ${attempted_array.join(", ")} )`;
+        user_number.value = "";
+        user_number.focus();
 });
 
 
 reset.addEventListener("click", () => {
-    counter = 0;
-    attempts.innerHTML = "Number of Attempts: " + counter;
+    counter = 7;
+    big_number = 100;
+    little_number = 0;
+    attempts.innerHTML = `You have the right to try ${counter} Times`;
     output_span.style.display = "none";
-    user_number.value = 0;
+    user_number.value = "";
+    number = myRandom();
+    console.log(number);
+    user_number.focus();
+    submit.disabled = false;
+    info.innerHTML = `Enter a number between ${little_number} and ${big_number}`;
+    reset.value = "Reset";
+    attempted_array = [];
 })
 
 
     //  web page load, auto focus
 
-user_number.focus();  
+user_number.focus(); 
+
+user_number.addEventListener("keydown", (e) => {
+    if (e.key == "Enter" || e.key == "NumpadEnter") {submit.click()};
+})
