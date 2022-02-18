938fdac5cfd72d23733587fecc7699df5c48f5d7
tarik <63872091+tariksitki@users.noreply.github.com>
2022-02-18 17:59:14 +0100

    completed

--- app.js
+++ app.js
@@ -18,44 +18,6 @@ let attempts = document.getElementById("attempts_span");
 let reset = document.getElementById("reset");
 let info = document.getElementById("info")
 
-// let counter = 0;
-
-// submit.addEventListener("click", () => {
-
-//     if (user_number.value == number) {
-//         counter += 1;
-//         output_span.style.display = "block";
-//         output_span.innerText = "Congrats!!! You found the number!";
-//         attempts.innerHTML = "Number of Attempts: " + counter;
-
-//     } else if (user_number.value > number) {
-//         output_span.style.display = "block";
-//         output_span.innerHTML = "Enter a smaller number!";
-//         counter += 1;
-//         attempts.innerHTML = "Number of Attempts: " + counter;
-
-//     } else if (user_number.value < number) {
-//         output_span.style.display = "block";
-//         output_span.innerText = "Enter a bigger number!";
-//         counter += 1;
-//         attempts.innerHTML = "Number of Attempts: " + counter;
-//     }  
-
-// });
-
-
-// reset.addEventListener("click", () => {
-//     counter = 0;
-//     attempts.innerHTML = "Number of Attempts: " + counter;
-//     output_span.style.display = "none";
-//     user_number.value = 0;
-// })
-
-
-//     //  web page load, auto focus
-
-// user_number.focus();  
-
 let counter = 7;
 let big_number = 100;
 let little_number = 0;
@@ -165,10 +127,53 @@ reset.addEventListener("click", () => {
     info.style.fontSize = "1.8rem";
 })
 
-
     //  web page load, auto focus
 
 user_number.focus(); 
 user_number.addEventListener("keydown", (e) => {
     if (e.key == "Enter" || e.key == "NumpadEnter") {submit.click()};
 })
+
+
+
+
+
+
+
+// let counter = 0;
+
+// submit.addEventListener("click", () => {
+
+//     if (user_number.value == number) {
+//         counter += 1;
+//         output_span.style.display = "block";
+//         output_span.innerText = "Congrats!!! You found the number!";
+//         attempts.innerHTML = "Number of Attempts: " + counter;
+
+//     } else if (user_number.value > number) {
+//         output_span.style.display = "block";
+//         output_span.innerHTML = "Enter a smaller number!";
+//         counter += 1;
+//         attempts.innerHTML = "Number of Attempts: " + counter;
+
+//     } else if (user_number.value < number) {
+//         output_span.style.display = "block";
+//         output_span.innerText = "Enter a bigger number!";
+//         counter += 1;
+//         attempts.innerHTML = "Number of Attempts: " + counter;
+//     }  
+
+// });
+
+
+// reset.addEventListener("click", () => {
+//     counter = 0;
+//     attempts.innerHTML = "Number of Attempts: " + counter;
+//     output_span.style.display = "none";
+//     user_number.value = 0;
+// })
+
+
+// web page load, auto focus
+
+// user_number.focus();  
